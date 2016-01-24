0e2ca042c24d53779effdc2ee128e51f3e3fc7f4
christop <christoph@lafkon.net>
2016-01-24 13:46:46 +0100

    keep non-cutup lines intact

--- utils/cutup/mkmdsh.sh
+++ utils/cutup/mkmdsh.sh
@@ -1,47 +1,69 @@
 #!/bin/bash
 
- EDIT="dev.txt"
- REF="REFERENCES.txt"
- MDSHACTION="% QUAVERBATIM"
+  EDIT="dev.txt"
+  REF="REMEMBER.txt"
+  MDSHACTION="% QUAVERBATIM"
 
 
- echo ""
- echo "% ------------------------- "
+ ( IFS=$'\n';NEWRANGE="Y";R=$RANDOM   # PRESET
 
- ( IFS=$'\n';CNT=0;PREV=0;
-
-  for LINE in  `cat $EDIT`
+  for LINE in  `cat $EDIT           | # DISPLAY $EDIT
+                sed 's/^[ \t]*$/X/' | # MAKE EMPTY X
+                grep -n ""`           # NUMBER LINES
    do
-     SRCID=`echo $LINE       | #
-            cut -d ":" -f 1`   #
-    SRCURL=`grep $SRCID $REF | #
-            head -n 1        | # 
-            cut -d ":" -f 2-`  #
-     LINUM=`echo $LINE       | #
-            cut -d ":" -f 2  | #
-            sed 's/[ ]*//g'`   #
+        N=`echo $LINE | cut -d ":" -f 1`  # STORE LINE NUMBER
+     LINE=`echo $LINE | cut -d ":" -f 2-` # AND REMOVE LINE NUMBER
 
-   # FOR THE VERY FIRST TIME           #
-   # --------------------------------- #
-   if [ $CNT -eq 0 ]; then
-        F=$LINUM; LINES="${LINUM}"
-    else
-     if [ "$LINUM" -eq `expr $PREV + 1` ]; then
-           LINES="${F}-${LINUM}"
+     if [ Y`echo $LINE                             | # ECHO $LINE
+            sed "s/^[a-f0-9]\{3\}:[0-9 ]*:.*$/$R/" | # REPLACE 'CUTUP' PATTERN WITH $R
+            grep "^${R}$"` == "Y${R}" ]              # IF MATCH
+     then
+             SRCID=`echo $LINE       | # DISPLAY LINE
+                    cut -d ":" -f 1`   # CUT FIELD 1 (= SRCID)
+            NEXTSRCID=`sed -n "$((N + 1))p" $EDIT | # DISPLAY LINE AFTER LINE
+                       cut -d ":" -f 1`             # CUT FIELD 1 (= SRCID)
+            SRCURL=`grep $SRCID $REF | # FIND SRCID IN REFERENCE
+                    head -n 1        | # FIRST LINE ONLY
+                    cut -d ":" -f 2-`  # CUT FIELD 1 (= SRCURL)
+            THISLINUM=`echo $LINE       | # DISPLAY LINE
+                       cut -d ":" -f 2  | # CUT FIELD 2 (= LINE NUMBER)
+                       sed 's/[ ]*//g'`   # REMOVE SPACES
+            NEXTLINUM=`sed -n "$((N + 1))p" $EDIT        | # DISPLAY LINE AFTER LINE
+                       cut -d ":" -f 2  | sed 's/[ ]*//g'` # CUT FIELD 2 / REMOVE SPACES
+
+            if [ R$NEWRANGE == RY ]; then
+                 F=$THISLINUM
+            fi
+
+         if [ Y`sed -n "$((N - 1))p" $EDIT             | # DISPLAY PREVIOUS LINE
+                sed "s/^[a-f0-9]\{3\}:[0-9 ]*:.*$/$R/" | # REPLACE 'CUTUP' PATTERN WITH $R
+                grep "^${R}$"` != "Y${R}" ]              # IF NO MATCH
+         then
+              echo "% ---------------- "
+         fi
+
+            if [ "X$NEXTLINUM" != X`expr $THISLINUM + 1` ] ||
+               [ "X$SRCID" != "X$NEXTSRCID" ]; then
+                  if [ $F -eq $THISLINUM ]; then
+                       echo "% $SRCURL $F"
+                  else
+                       echo "% $SRCURL ${F}-${THISLINUM}"
+                  fi
+                  NEWRANGE="Y"
+            else
+                  NEWRANGE="N"
+            fi
+
+         if [ Y`sed -n "$((N + 1))p" $EDIT             | # DISPLAY NEXT LINE
+                sed "s/^[a-f0-9]\{3\}:[0-9 ]*:.*$/$R/" | # REPLACE 'CUTUP' PATTERN WITH $R
+                grep "^${R}$"` != "Y${R}" ]              # IF NO MATCH
+         then
+              echo "${MDSHACTION}:"
+         fi
      else
-           echo "% $SRCURL ${LINES}"           
-           F=$LINUM
-           LINES="${LINUM}"
+          echo $LINE | sed 's/^X$//g'
      fi
-    fi
-
-     PREV="$LINUM"
-     CNT=`expr $CNT + 1`
  done
-
- echo "% $SRCURL ${LINES}"
- echo "${MDSHACTION}:"
- echo ""
 
  )
 
